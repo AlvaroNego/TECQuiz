86a4f779ebb5383dd5f93c0b046a75f4abe3b205
AlvaroNego <63549379+AlvaroNego@users.noreply.github.com>
2025-09-03 21:08:24 -0300

    v1.3.1
    removed cors

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template, request, jsonify, send_file
-from flask_cors import CORS
 import json
 import os
 
 app = Flask(__name__)
-CORS(app)  # Permite todas as origens por padrão
+
 
 CADASTROS_FILE = os.path.join(os.path.dirname(__file__), "cadastros.json")
 TOKEN = '2005'
